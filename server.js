73343052cdd7d9f55c433cd6d11a8317569a7058
Kate Dubitski <kate.dubitski@gmail.com>
2023-05-11 14:06:00 -0400

    add HTTP server

---
+++ server.js
@@ -0,0 +1,50 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Serve static files
+  const filePath = path.join(__dirname, req.url);
+  const contentType = getContentType(filePath);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        res.statusCode = 404;
+        res.end('File not found');
+      } else {
+        res.statusCode = 500;
+        res.end('Internal server error');
+      }
+    } else {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', contentType);
+      res.end(data);
+    }
+  });
+});
+
+const port = 3000; // Choose a port number
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+function getContentType(filePath) {
+  const extname = path.extname(filePath);
+  switch (extname) {
+    case '.html':
+      return 'text/html';
+    case '.css':
+      return 'text/css';
+    case '.js':
+      return 'text/javascript';
+    default:
+      return 'text/plain';
+  }
+}
